d6d4a6df7135aaccb3ded48c3f0f8dc0151b123f
Lucas Fuentes <lucasfuentes100@hotmail.com>
2021-06-22 15:34:38 -0700

    changing the way to handle initial conditions

--- src/dyn_model.jl
+++ src/dyn_model.jl
@@ -83,11 +83,12 @@ function DynPowerManagementProblem(
     n, _ = size(A)
 
     # Define a storage variable for n nodes over T timesteps
-    s = Variable(n, T+1)
+    # TODO: for now the edge constraints are neglected
+    s = Variable(n, T)
     add_constraints!(s,[
-        0 <= s <= C, 
-        s[:, 1] == 0, 
-        s[:, T+1] == 0
+        0 <= s <= C
+        # s[:, 1] == 0, 
+        # s[:, T+1] == 0
     ])
     add_constraints!(s, [
         s[t+1] - s[t] <= P for t in 1:T
@@ -97,9 +98,14 @@ function DynPowerManagementProblem(
     ])
 
     subproblems = [
+        # first treating the initial constraint explicitly to avoid having
+        # specify another variable for s_0
         PowerManagementProblem(
-            fq[t], fl[t], d[t], pmax[t], gmax[t], A, (s[t+1] - s[t])
-            ) for t in 1:T
+            fq[t], fl[t], d[t], pmax[t], gmax[t], A, (s[1] - C)
+            ), 
+        PowerManagementProblem(
+            fq[t], fl[t], d[t], pmax[t], gmax[t], A, (s[t] - s[t-1])
+            ) for t in 2:T
         ]
 
     objective = sum([sub.problem.objective for sub in subproblems])
@@ -139,7 +145,7 @@ get_lmps(P::PowerManagementProblem) =
 
 
 # ===
-# KKT OPERATOR
+# KKT OPERATOR for the PMP
 # ===
 
 """
@@ -181,6 +187,72 @@ function flatten_variables(P::PowerManagementProblem)
 end
 
 function unflatten_variables(x, n, m)
+    i = 0
+    
+    g =  x[i+1:i+n]
+    i += n
+    
+    p = x[i+1:i+m]
+    i += m
+    
+    λpl = x[i+1:i+m]
+    i += m
+    λpu = x[i+1:i+m]
+    i += m
+    
+    λgl = x[i+1:i+n]
+    i += n
+    λgu = x[i+1:i+n]
+    i += n
+    
+    ν = x[i+1:i+n]
+    
+    return g, p, λpl, λpu, λgl, λgu, ν
+end
+
+# ===
+# KKT OPERATOR for the DynPMP
+# ===
+
+"""
+    kkt_dims(n, m)
+
+Compute the dimensions of the input / output of the KKT operator for
+a network with `n` nodes and `m` edges.
+"""
+kkt_dims_dyn(n, m) = 4n + 3m
+
+"""
+    kkt(x, fq, fl, d, pmax, gmax, A; τ=TAU)
+
+Compute the KKT operator applied to `x`, with parameters given by `fq`,
+`fl`, `d`, `pmax`, `gmax`, `A`, and `τ`.
+"""
+function kkt_dyn(x, fq, fl, d, pmax, gmax, A; τ=TAU)
+    n, m = size(A)
+
+    g, p, λpl, λpu, λgl, λgu, ν = unflatten_variables(x, n, m)
+
+    # Lagragian is
+    # L = J + λpl'(-p - pmax) + ... + λgu'(g - gmax) + v'(Ap - g - d)
+    return [
+        Diagonal(fq)*g +  fl - ν - λgl + λgu; 
+        A'ν + λpu - λpl + τ*p;
+        λpl .* (-p - pmax);
+        λpu .* (p - pmax);
+        -λgl .* g;
+        λgu .* (g - gmax);
+        A*p - g + d;
+    ]
+end
+
+function flatten_variables_dyn(P::PowerManagementProblem)
+    x = [evaluate(P.g); evaluate(P.p)]
+    λ = vcat([c.dual for c in P.problem.constraints]...)
+    return [x; λ][:, 1]
+end
+
+function unflatten_variables_dyn(x, n, m)
     i = 0
     
     g =  x[i+1:i+n]
