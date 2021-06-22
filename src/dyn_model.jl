796d51b71b8056321168d0a5f55977261e696da7
Lucas Fuentes <lucasfuentes100@hotmail.com>
2021-06-22 16:40:36 -0700

    laying the foundation for the dynamical networks

--- src/dyn_model.jl
+++ src/dyn_model.jl
@@ -6,6 +6,7 @@
 # ===
 
 TAU = 1e-5
+INIT_COND = 0
 
 mutable struct PowerManagementProblem
     problem::Problem
@@ -84,7 +85,7 @@ function DynPowerManagementProblem(
 
     # Define a storage variable for n nodes over T timesteps
     # TODO: for now the edge constraints are neglected
-    s = Variable(n, T)
+    s = [Variable(n) for t in 1:T]
     add_constraints!(s,[
         0 <= s <= C
         # s[:, 1] == 0, 
@@ -101,7 +102,7 @@ function DynPowerManagementProblem(
         # first treating the initial constraint explicitly to avoid having
         # specify another variable for s_0
         PowerManagementProblem(
-            fq[t], fl[t], d[t], pmax[t], gmax[t], A, (s[1] - C)
+            fq[t], fl[t], d[t], pmax[t], gmax[t], A, (s[1] - INIT_COND)
             ), 
         PowerManagementProblem(
             fq[t], fl[t], d[t], pmax[t], gmax[t], A, (s[t] - s[t-1])
@@ -153,6 +154,8 @@ get_lmps(P::PowerManagementProblem) =
 
 Compute the dimensions of the input / output of the KKT operator for
 a network with `n` nodes and `m` edges.
+
+TODO: update
 """
 kkt_dims(n, m) = 4n + 3m
 
@@ -160,7 +163,10 @@ kkt_dims(n, m) = 4n + 3m
     kkt(x, fq, fl, d, pmax, gmax, A; τ=TAU)
 
 Compute the KKT operator applied to `x`, with parameters given by `fq`,
-`fl`, `d`, `pmax`, `gmax`, `A`, and `τ`.
+`fl`, `d`, `pmax`, `gmax`, `A`, and `τ`. `x` contains both the primal
+and the dual variables.
+
+Those are interpreted as the KKTs for a given timestep
 """
 function kkt(x, fq, fl, d, pmax, gmax, A; τ=TAU)
     n, m = size(A)
@@ -180,12 +186,19 @@ function kkt(x, fq, fl, d, pmax, gmax, A; τ=TAU)
     ]
 end
 
+
+"""
+combine primal and dual variables after solving
+"""
 function flatten_variables(P::PowerManagementProblem)
     x = [evaluate(P.g); evaluate(P.p)]
     λ = vcat([c.dual for c in P.problem.constraints]...)
     return [x; λ][:, 1]
 end
 
+"""
+extract the primal/dual variables from x
+"""
 function unflatten_variables(x, n, m)
     i = 0
     
@@ -215,63 +228,80 @@ end
 # ===
 
 """
-    kkt_dims(n, m)
+    kkt_dims_dyn(n, m,T)
 
 Compute the dimensions of the input / output of the KKT operator for
-a network with `n` nodes and `m` edges.
+a network with `n` nodes and `m` edges for `T` timesteps
 """
-kkt_dims_dyn(n, m) = 4n + 3m
+kkt_dims_dyn(n, m, T) = T*(9n+3m)
 
 """
     kkt(x, fq, fl, d, pmax, gmax, A; τ=TAU)
 
 Compute the KKT operator applied to `x`, with parameters given by `fq`,
 `fl`, `d`, `pmax`, `gmax`, `A`, and `τ`.
+
+
+WIP/TODO:
+---------
+The steps should be:
+- compute the KKTs for individual problems
+- include the KKTs linked to S_t
+- include the KKTs linked to the coupling between different timesteps
 """
-function kkt_dyn(x, fq, fl, d, pmax, gmax, A; τ=TAU)
+function kkt_dyn(x, fq, fl, d, pmax, gmax, A, P, C; τ=TAU)
+
     n, m = size(A)
+    T = length(fq)
 
-    g, p, λpl, λpu, λgl, λgu, ν = unflatten_variables(x, n, m)
+    # Assume that it is giving back what we want for now
+    # the format we assume is that each variable (both primal and dual)
+    # is returned as an array of T elements
+    # λdsi is the dual for charge/discharge of the battery, with i being 1 or 2
+    g, p, s, λpl, λpu, λgl, λgu, λds1, λds2, λsl, λsu, ν = unflatten_variables_dyn(x, n, m, T)
 
-    # Lagragian is
-    # L = J + λpl'(-p - pmax) + ... + λgu'(g - gmax) + v'(Ap - g - d)
+    # compute the KKTs for individual problems
+    KKT_tot = []
+    for t in 1:T
+        x = [g[t]; p[t]; λpl[t]; λpu[t]; λgl[t]; λgu[t]; ν[t]]
+        KKT = kkt(x, fq[t], fl[t], d[t], pmax[t], gmax[t], A; τ=TAU)
+        if t == 1
+            ds = s[t] - INIT_COND
+        else
+            ds = s[t] - s[t-1]
+        end
+        KKT_s = kkt_storage(λds1[t], λds2[t], λsl[t], λsu[t], ds, s[t]) 
+        KKT_tot = vcat(KKT_tot, KKT, KKT_s) # append the subproblem kkt matrix to the total KKT matrix
+    end
+
+    return KKT_tot
+end
+
+"""
+Compute the terms kkt matrix that are only related to the storage
+"""
+function kkt_storage(λds1, λds2, λsl, λsu, ds, s)
+    
     return [
-        Diagonal(fq)*g +  fl - ν - λgl + λgu; 
-        A'ν + λpu - λpl + τ*p;
-        λpl .* (-p - pmax);
-        λpu .* (p - pmax);
-        -λgl .* g;
-        λgu .* (g - gmax);
-        A*p - g + d;
+        λds1 .* (ds - P);
+        λds2 .* (-ds - P);
+        λsl .* (-s);
+        λsu .* (s - C)
     ]
 end
 
+"""
+add doc
+"""
 function flatten_variables_dyn(P::PowerManagementProblem)
     x = [evaluate(P.g); evaluate(P.p)]
     λ = vcat([c.dual for c in P.problem.constraints]...)
     return [x; λ][:, 1]
 end
 
-function unflatten_variables_dyn(x, n, m)
-    i = 0
-    
-    g =  x[i+1:i+n]
-    i += n
-    
-    p = x[i+1:i+m]
-    i += m
-    
-    λpl = x[i+1:i+m]
-    i += m
-    λpu = x[i+1:i+m]
-    i += m
-    
-    λgl = x[i+1:i+n]
-    i += n
-    λgu = x[i+1:i+n]
-    i += n
-    
-    ν = x[i+1:i+n]
-    
-    return g, p, λpl, λpu, λgl, λgu, ν
+"""
+add doc
+"""
+function unflatten_variables_dyn(x, n, m, T)
+    #TODO
 end
---
+++ src/test_dyn.jl
@@ -0,0 +1,2 @@
+# script to test the dynamic networks and the computation of KKT conditions
+# to be erased when the code works
