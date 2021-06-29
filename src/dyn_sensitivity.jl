8deb5b83f37d6d520e885bb56a74624964681140
Lucas Fuentes <lucasfuentes100@hotmail.com>
2021-06-28 17:31:36 -0700

    sensitivity to demand

---
+++ src/dyn_sensitivity.jl
@@ -0,0 +1,16 @@
+# Computing sensitivities in the dynamic PMP setting
+
+function sensitivity_demand_dyn(P::PowerManagementProblem, net::DynamicPowerNetwork, d, ∇C)
+
+    x = flatten_variables_dyn(P)
+
+    # Get partial Jacobians of KKT operator
+    _, ∂K_xT = Zygote.forward_jacobian(x -> kkt_dyn(x, net, d), x)
+    _, ∂K_θT = Zygote.forward_jacobian(d -> kkt(x, net, d), d)
+
+    # Now compute ∇C(g*(θ)) = -∂K_θ' * (∂K_x' * v)
+    v = ∂K_xT \ ∇C
+    ∇C_θ = -∂K_θT * v
+
+    return ∇C_θ
+end
