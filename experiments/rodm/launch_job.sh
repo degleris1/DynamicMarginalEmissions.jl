039044a1e7a3e01fe0e6682a668c1feb476a114a
degleris1 <degleris1@gmail.com>
2021-09-28 12:57:09 -0700

    made slurm script multithread

--- experiments/rodm/launch_job.sh
+++ experiments/rodm/launch_job.sh
@@ -4,7 +4,7 @@
 #
 #SBATCH --time=01:00:00
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=8
 #SBATCH --mem-per-cpu=8G
 
 SCIPT_DIR=/home/users/degleris/CarbonNetworks.jl/experiments/rodm/
@@ -12,4 +12,4 @@ SCIPT_DIR=/home/users/degleris/CarbonNetworks.jl/experiments/rodm/
 srun hostname
 module load julia
 
-srun julia ${SCIPT_DIR}${1}
+srun julia -t 8 ${SCIPT_DIR}${1}
