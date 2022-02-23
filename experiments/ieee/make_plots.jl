d7691094116b6a69d97dd15c172788f1dd2d1c80
degleris1 <degleris1@gmail.com>
2022-02-23 09:33:44 -0800

    updated ieee plots

--- experiments/ieee/make_plots.jl
+++ experiments/ieee/make_plots.jl
@@ -1,16 +1,15 @@
 ### A Pluto.jl notebook ###
-# v0.17.1
+# v0.17.7
 
 using Markdown
 using InteractiveUtils
 
 # ╔═╡ 85394d0a-6f17-11ec-04e7-89c834098e0c
 begin
-	using Pkg; Pkg.activate("")
+	using Pkg; Pkg.activate(joinpath(@__DIR__, "../../dev"))
 	
 	using HDF5
 	using CairoMakie
-	using CarbonNetworks
 	using ColorSchemes
 	using Colors
 end
@@ -20,19 +19,16 @@ using ColorSchemeTools
 
 # ╔═╡ 177d72ea-5a88-416e-99ee-4ed694b0384e
 begin
-#Anthony
-DATA_PATH = "/Users/degleris/Data/carbon_networks/fig2_data.h5"
-SAVE_PATH = "/Users/degleris/Documents/Research/CarbonNetworks/img/"
-
+	#Anthony
+	DATA_DIR = "/Users/degleris/Data/carbon_networks/"
+	SAVE_PATH = "/Users/degleris/Documents/Research/CarbonNetworks/img/"
 	
-#Lucas
-DATA_PATH = "/Users/lucasfuentes/sensitivity/results/fig2_data"
-SAVE_PATH = "/Users/lucasfuentes/sensitivity/img/"
-
-
-fnm1 = "network_mefs.pdf"
-fnm2 = "mef_heatmaps.pdf"
-
+	#Lucas
+	# DATA_DIR = "/Users/lucasfuentes/sensitivity/results/"
+	# SAVE_PATH = "/Users/lucasfuentes/sensitivity/img/"
+	
+	fnm1 = "network_mefs.pdf"
+	fnm2 = "mef_heatmaps.pdf"
 end;
 
 # ╔═╡ c041f8ff-df9a-4b6a-a73b-b125c3de4495
@@ -40,17 +36,38 @@ md"""
 ### Load data
 """
 
+# ╔═╡ 6986e19a-c052-4e0f-b765-46496e2c8a36
+function load_h5(path)
+	return h5open(path) do f
+		r = Dict()
+
+		for k in keys(f)
+			r[k] = read(f[k])
+		end
+
+		r
+	end
+end
+
 # ╔═╡ 9874d30f-c591-48a4-8118-a523bc758509
-# f = h5open("/Users/degleris/Data/carbon_networks/fig2_data.h5") do f
-f = h5open("/Users/lucasfuentes/sensitivity/results/fig2_data") do f
+f = load_h5(joinpath(DATA_DIR, "fig2_data_1"));
+
+# ╔═╡ be52012c-4a74-4b3d-bdaf-ce380c8b08b0
+curve_nodes = [3, 21, 24]
+
+# ╔═╡ 49af4bc2-be22-48b7-a08f-e5db775a64dc
+curve_datasets = let
 	r = Dict()
 
-	for k in keys(f)
-		r[k] = read(f[k])
+	for k in curve_nodes
+		r[k] = load_h5(joinpath(DATA_DIR, "fig2_data_sensitivity_node_$k"))
 	end
 
 	r
-end;
+end
+
+# ╔═╡ 48d56784-63e1-408e-8b0b-0dc1d858b836
+diff_pts = collect(95:5:105)
 
 # ╔═╡ c33a5eb8-ae22-412c-90ef-9cb1ab385ecb
 md"""
@@ -60,34 +77,45 @@ md"""
 # ╔═╡ 672984c0-eff8-4156-9ce8-0d2f919a4d4c
 figure_nodal_mefs = let
 	nodal_mefs = f["MEFs"] / 1000
-	Δd = f["exp_vs_th_x"]
-	ΔE = f["exp_vs_th_y_exp"]
-	ΔE_est = f["exp_vs_th_y_th"]
 
 	# Setup
-	size_inches = (6.5, 2)
+	size_inches = (6.5, 4)
 	size_pt = 72 .* size_inches
 	fig = Figure(resolution=size_pt ./ 0.75, fontsize=10)
 
-	# Left Panel
-	ax_left = Axis(fig[1, 1], xgridvisible=false, ygridvisible=false)
-	barplot!(ax_left, nodal_mefs, strokewidth=0.5)
-	xlims!(ax_left, 0, 31)
-	ax_left.ylabel = "MEF [mTCO2 / MWh]"
-	ax_left.xlabel = "Node"
+	# Top panels
+	axes_top = [Axis(fig[1, k], xgridvisible=false, ygridvisible=false) for k in 1:3]
+	for (ind_node, node) in enumerate(curve_nodes)
+		ax = axes_top[ind_node]
+		data = curve_datasets[node]
+		x = data["x"]
+		
+		lines!(ax, x, data["y_exp"], label="True", linewidth=3)
+		for d in diff_pts
+			lines!(ax, x, data["y_$d"], label="Estimates", 
+				linestyle=:dash, linewidth=2, color=:red)
+			scatter!(ax, [d/100], [data["y_$d"][findfirst(==(d/100), x)]],
+				color=(:red, 0.75), markersize=8)
+		end
+		
+		xlims!(ax, 0.9, 1.1)
+		ylims!(ax, 0.98, 1.09)
+		ax.ylabel = "ΔE [%]"
+		ax.xlabel = "ΔD [%]"
+		ax.title = "Node $node"
+		axislegend(ax, position=:lt, padding=(4.0, 4.0, 2.0, 2.0), rowgap=0, unique=true)
+	end
 
-	# Right Panel
-	ax_right = Axis(fig[1, 2], xgridvisible=false, ygridvisible=false)
-	lines!(ax_right, Δd, ΔE, label="True", linewidth=3)
-	lines!(ax_right, Δd, ΔE_est, label="Estimate", linestyle=:dot, linewidth=3)
-	xlims!(ax_right, 0.9, 1.1)
-	ylims!(ax_right, 0.98, 1.06)
-	ax_right.ylabel = "ΔE [%]"
-	ax_right.xlabel = "ΔD [%]"
-	axislegend(ax_right, position=:lt, padding=(4.0, 4.0, 2.0, 2.0), rowgap=0)
+
+	# Bottom panel
+	ax_bot = Axis(fig[2, 1:3], xgridvisible=false, ygridvisible=false)
+	barplot!(ax_bot, nodal_mefs, strokewidth=0.5)
+	xlims!(ax_bot, 0, 31)
+	ax_bot.ylabel = "MEF [mTCO2 / MWh]"
+	ax_bot.xlabel = "Node"
 
 	# Add panel labels
-	for (label, layout) in zip(["A", "B"], [fig[1, 1], fig[1, 2]])
+	for (label, layout) in zip(["A", "B"], [fig[1, 1], fig[2, 1]])
 	    Label(
 			layout[1, 1, TopLeft()], 
 			label,
@@ -98,9 +126,6 @@ figure_nodal_mefs = let
 		)
 	end
 
-	# Decrease size of second column
-	colsize!(fig.layout, 2, Auto(0.5))
-
 	fname = joinpath(SAVE_PATH, fnm1)
 	save(fname, fig, pt_per_unit=0.75)
 	fig
@@ -110,6 +135,9 @@ end
 md"""
 ### Plot B: MEFs over time
 """
+
+# ╔═╡ 274013ad-c939-467f-8dfd-cc199340bd2c
+storage_pcs = ["0% Storage", "5% Storage", "10% Storage"]
 
 # ╔═╡ 2c443306-d607-42ff-b36a-4822b61e0846
 # http://juliagraphics.github.io/Colors.jl/stable/namedcolors/
@@ -130,96 +158,86 @@ figure_temporal = let
 	
 	colormap = cgrad(
 		[:firebrick4, :orangered2,  :grey96, :skyblue1, :navyblue], 
-		[.3, .49, (0-cmin)/(cmax-cmin), .51, .8]
+		[.3, .49, (0-cmin)/(cmax-cmin), .51, .8],
+		rev=true
 	)
-	# colormap=:balance
 	
 	# Setup
 	lw = 3
-	size_inches = (6.5, 3)
+	size_inches = (6.5, 4)
 	size_pt = 72 .* size_inches
 	
 	fig = Figure(resolution=size_pt ./ 0.75, fontsize=10)
-	#grid_left = fig[1, 1] = GridLayout()
-	#grid_right = fig[1, 2] = GridLayout()
-
-	
-	# Left panel
-	gl_axes = [
-		Axis(fig[k, 1], xgridvisible=false, ygridvisible=false) 
-		for k in 1:2
-	]
-	for i in 1:2
-		xlims!(gl_axes[i], 1, 24)
-		gl_axes[i].xticks = 0:6:24
-		
-		lines!(gl_axes[i], total_mefs[i][:, 1], linewidth=lw)
-		lines!(gl_axes[i], total_mefs[i][:, 2], linewidth=lw)
-	end
-	Label(fig[1:2, 1, Left()], "MEF [mTCO2 / MWh]", 
-		rotation=pi/2, 
-		padding=(0, 35, 5, 0)
-	)
-	gl_axes[2].xlabel = "Hour"
-
 	
 	# Right panel
-	gr_axes = [
-		Axis(fig[j, k+1]) 
+	subplts = [
+		fig[j, k] = GridLayout()
 		for j in 1:2, k in 1:3
 	]
 	for (j, k) in Iterators.product(1:2, 1:3)
+		M = f["hm_$(nodes[j])_$(k)"]./1000
+		m = sum(M, dims=1)[1, :]
+
+		ax_top = Axis(subplts[j, k][1, 1], ygridvisible=false, xgridvisible=false)
+		ax_main = Axis(subplts[j, k][2, 1])
+		linkxaxes!(ax_top, ax_main)
+
+		xlims!(ax_top, 1, 24)
+		if j == 1
+			ylims!(ax_top, -1, 1)
+			ax_top.yticks = [0, 1]
+		else
+			ylims!(ax_top, -1, 3.5)
+			ax_top.yticks = [0, 3]
+		end
 		
+		band!(ax_top, hours, zeros(24), m, linewidth=lw, color=(m .> 0) .- 0.5, colormap=colormap, colorrange=(-1, 1))
+		ax_top.xticks = [-100]
+		if j == 1
+			ax_top.title = storage_pcs[k]
+		end
+
 		heatmap!(
-			gr_axes[j, k], f["hm_$(nodes[j])_$(k)"]./1000, 
-			colorrange=(cmin, cmax), colormap=colormap
+			ax_main, M, 
+			colorrange=(cmin, cmax), colormap=colormap,
 		)
-		gr_axes[j, k].xticks = 0:6:24
-		gr_axes[j, k].yticks = 0:6:24
+		ax_main.xticks = 0:6:24
+		ax_main.yticks = 0:6:24
+		
 
 		if k != 1
-			gr_axes[j, k].ytickformat = xs -> ["" for x in xs]
+			ax_main.ytickformat = xs -> ["" for x in xs]
 		end
 		if j != 2
-			gr_axes[j, k].xtickformat = xs -> ["" for x in xs]
+			ax_main.xtickformat = xs -> ["" for x in xs]
 		end
+
+		rowgap!(subplts[j, k], 5)
+		rowsize!(subplts[j, k], 1, Auto(1))
+		rowsize!(subplts[j, k], 2, Auto(4))
+
+		if k == 1
+			Label(subplts[j, k][1, 1, Left()], "Total", 
+				rotation=pi/2, padding=(0, 30, 0, 0))
+			Label(subplts[j, k][2, 1, Left()], "Emissions Time", 
+				rotation=pi/2, padding=(0, 30, 0, 0))
+		end
+
 	end
 	cb = Colorbar(
-		fig[1:2, 5], 
+		fig[1:2, 4], 
 		colormap = colormap, 
 		limits=(cmin, cmax), 
-		label="MEF [mTCO2 / MWh]"
+		label="MEF [TCO2 / MWh]"
 	)
 	
-	Label(fig[1:2, 2, Left()], "Emissions Time", 
-		rotation=pi/2, 
-		padding=(0, 30, 0, 0)
-	)
-	Label(fig[2, 2:4, Bottom()], "Consumption Time", 
-		padding=(0, 0, 0, 24)
-	)
-
-	
-	# Tweaks
-	# Grow left figure column
-	colsize!(fig.layout, 1, Auto(3))
-	
-	# Make heatmaps square
-	for j in 2:4
-		colsize!(fig.layout, j, Aspect(1, 1))
+	for j in 1:2
+		Label(fig[j, 1, Left()], "Node $(nodes[j])", 
+			rotation=pi/2, padding=(0, 50, 0, 0))
 	end
+	
+	Label(fig[2, 1:3, Bottom()], "Consumption Time", padding=(0, 0, 0, 24))
 
-	# Add panel labels
-	for (label, layout) in zip(["A", "B"], [fig[1, 1], fig[1, 2]])
-	    Label(
-			layout[1, 1, TopLeft()], 
-			label,
-	        textsize=18,
-			font="Noto Sans Bold",
-	        padding=(0, 5, 5, 0),
-	        halign=:right
-		)
-	end
 
 	
 	
@@ -235,11 +253,16 @@ maximum(maximum.([f["hm_$(Dict(1=>21, 2=>23)[j])_$(k)"] for j in 1:2, k in 1:3])
 # ╠═85394d0a-6f17-11ec-04e7-89c834098e0c
 # ╠═177d72ea-5a88-416e-99ee-4ed694b0384e
 # ╟─c041f8ff-df9a-4b6a-a73b-b125c3de4495
+# ╟─6986e19a-c052-4e0f-b765-46496e2c8a36
 # ╠═9874d30f-c591-48a4-8118-a523bc758509
+# ╠═be52012c-4a74-4b3d-bdaf-ce380c8b08b0
+# ╠═49af4bc2-be22-48b7-a08f-e5db775a64dc
+# ╠═48d56784-63e1-408e-8b0b-0dc1d858b836
 # ╟─c33a5eb8-ae22-412c-90ef-9cb1ab385ecb
 # ╠═672984c0-eff8-4156-9ce8-0d2f919a4d4c
 # ╟─b6cf83e6-043e-452a-ba72-2e5911c0cffa
 # ╠═00c874fc-f087-4c6a-b653-feb5080ff89d
+# ╠═274013ad-c939-467f-8dfd-cc199340bd2c
 # ╠═2c443306-d607-42ff-b36a-4822b61e0846
 # ╠═4e579820-e404-4a44-900f-cbd588dd931e
 # ╠═0b004661-a463-4f00-bad9-3532245000da
