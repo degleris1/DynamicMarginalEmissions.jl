046ac164a9cc3fad0494e0ca069c0d5d6a0e5688
Lucas Fuentes <lucasfuentes100@hotmail.com>
2022-03-02 13:46:33 -0800

    setting the structure for the dynamic problem

---
+++ experiments/wecc240/run_dynamic_model.jl
@@ -0,0 +1,58 @@
+include("util.jl")
+
+using Dates
+using BSON
+using CarbonNetworks
+
+DATES = Date(2004, 01, 01) .+ Day.(0:365)
+HOURS = 1:24
+
+
+function formulate_and_solve_dynamic(hour, day, month, T; Z=1e3, line_max=50_000.0, line_weight=1.3)
+    case, _ = make_dynamic_case(hour, day, month, T)
+
+    # Construct flow matrix
+    F = make_pfdf_matrix(case.A, case.β)
+
+    # Get generator costs
+    # TODO: check whether the fuel costs change over time
+    fl = [get_costs(case.heat, case.fuel, FUEL_COSTS) for _ in 1:T]
+    fq = [zeros(length(fl[k])) for k in 1:length(fl)]
+
+    # Get line capacities
+    # (Specifically, increase them a little bit)
+    pmax = [min.(line_weight * case.fmax, line_max) / Z for _ in 1:T]
+    gmax = case.gmax / Z
+    d = case.d / Z
+    # TODO: probably need to divide C and P etc. by Z, too! Check. 
+
+    # Formulate problem
+    net = DynamicPowerNetwork(
+        fq, fl, pmax, gmax, case.A, case.B, F, 
+        case.P, case.C, T; η_c=case.η, η_d=case.η
+        )
+    pmp = DynamicPowerManagementProblem(net, d)
+
+    # Solve
+    solve!(pmp, CarbonNetworks.OPT)
+    g = CarbonNetworks.evaluate(pmp.g)
+
+    # Get generator emissions rates
+    co2_rates = get_costs(case.heat, case.fuel, FUEL_EMISSIONS)
+
+    # Compute MEFs
+    λ = compute_mefs(pmp, net, d, co2_rates)
+
+    @show (hour, day, month, pmp.problem.status)
+
+    return (d=d, gmax=gmax, g=g, λ=λ, status=pmp.problem.status)
+end
+
+# case, meta = make_dynamic_case(1, 1, 1, 1)
+# results = [formulate_and_solve_static(h, day(d), month(d)) for h in HOURS, d in DATES]
+results = formulate_and_solve_dynamic(1, 1, 1, 1)
+
+# bson(
+#     "/Users/degleris/Data/carbon_networks/wecc240_static_results.bson", 
+#     case=case, meta=meta, results=results
+# )
