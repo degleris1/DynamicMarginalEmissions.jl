b5270db627d09c9f03bf1d360d26ae79d7f27378
degleris1 <degleris1@gmail.com>
2022-04-13 12:08:09 -0700

    reconfiguring for multiple runs (so we can run this on slurm)

--- experiments/wecc240/run.jl
+++ experiments/wecc240/run.jl
@@ -1,25 +1,30 @@
 include("util.jl")
 
-SETUP_FILE = (length(ARGS) > 0) ? ARGS[1] : nothing
+SETUP_FILE = (length(ARGS) >= 1) ? ARGS[1] : nothing
+RUN_NUMBER = (length(ARGS) >= 2) ? parse(Int, ARGS[2]) : "all"
 
-function run(setup_file::String)
+function run(setup_file::String, run_number)
     # Constants
     HOURS_PER_DAY = 24
     RESULTS_SUBFOLDER = "results240"
     DATE_FORMAT = "yy-mm-dd-HH"
+
+
+
 
     # Load run configuration
     setup_file = joinpath(@__DIR__, setup_file)
     setup = TOML.parsefile(setup_file)
 
     run_id = setup["RUN_ID"]
+    flag = setup["FLAG"]
+
     start_date = DateTime(setup["START_DATETIME"], DATE_FORMAT)
     num_days = setup["NUM_DAYS"]
     T = setup["TIME_HORIZON"]
 
-    # Verify inputs
-    @assert 1 <= num_days <= 364  # NREL is missing 7 hours of data, so we can run at most 364 days
-    @assert 1 <= T
+
+
 
     # Decide whether or not to use static or dynamic runs
     load_case = (T == 1) ? make_static_case : (d -> make_dynamic_case(d, T))
@@ -28,30 +33,63 @@ function run(setup_file::String)
     # Create dates
     dates = start_date .+ Hour.(0 : T : (HOURS_PER_DAY*num_days-1))
 
+
+
+
+    # Verify inputs
+    @assert flag in ["", "clean"]
+    if flag == "clean"
+        @assert run == "all"
+    end
+    @assert 1 <= num_days <= 364  # NREL is missing 7 hours of data, so we can run at most 364 days
+    @assert 1 <= T
+
     @assert all(d -> year(d) in [2004, 2018], dates)
     @assert all(d -> dayofyear(d) <= 364, dates)
+    @assert (run_number == "all") || (0 <= run_number <= length(dates))
+
+
+
 
     # Set up directory
     results_path = joinpath(SAVE_DIR, RESULTS_SUBFOLDER, run_id)
-    if ispath(results_path)
-        @warn "Path $results_path already exists: you may be overwriting data."
-    end
-    mkpath(results_path)
     rp = f -> joinpath(results_path, f)
 
-    # Put config file in directory for posterity
-    cp(setup_file, rp("setup.toml"), force=true)
+    if (run_number == "all") || (run_number == 0)
+        # Check if we are overwriting data
+        if ispath(results_path)
+            @warn "Path $results_path already exists: you may be overwriting data."
 
-    # Save a single case for later analyses
-    case = load_case(first(dates))
-    bson(rp("case.bson"); case...)
+            # If clean flag is passed, remove the directory and start from scratch
+            if flag == "clean"
+                rm(results_path, recursive=true)
+            end
+        end
 
-    for d in dates
-        results_d = run_model(d)
-        name_d = Dates.format(d, DATE_FORMAT) * ".bson"
+        # Create directory if it doesn't exist already
+        mkpath(results_path)
 
-        bson(rp(name_d); results_d...)
+        # Put config file in directory for posterity
+        cp(setup_file, rp("setup.toml"), force=true)
+
+        # Save a single case for later analyses
+        case = load_case(first(dates))
+        bson(rp("case.bson"); case...)
+    end
+
+
+
+    # Now actually run something!
+    run_and_save = d -> bson(rp(Dates.format(d, DATE_FORMAT) * ".bson"); run_model(d)...)
+
+    if run_number == "all"  # Run all cases
+        run_and_save.(dates)
+        return
+    elseif run_number == 0  # Just initialize the directory
+        return
+    else  # run_number > 0
+        run_and_save(dates[run_number])
     end
 end
 
-run(SETUP_FILE)
+run(SETUP_FILE, RUN_NUMBER)
